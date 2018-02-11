240011c995f62e9c6a0e201f13992c96ae23a69a
alekz29 <konfucjussz29@gmail.com>
2018-02-11 21:46:38 +0100

    creating sprites(fruits) using class

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as PIXI from 'pixi.js';
 import textureToCache from './textureToCache'
+import Fruit from './fruit'
 
 
 const renderer = PIXI.autoDetectRenderer(800, 600, {backgroundColor: 0x1099bb}),
@@ -8,11 +9,9 @@ const renderer = PIXI.autoDetectRenderer(800, 600, {backgroundColor: 0x1099bb}),
 textureToCache()
 document.body.appendChild(renderer.view);
 
+new Fruit(5,stage)
+new Fruit(8,stage)
 
-const sprite = PIXI.Sprite.fromFrame("29");
-sprite.x = Math.random() * 800 - 20
-sprite.y = 150
-stage.addChild(sprite)
 
 
 startGame();
--- src/fruit.ts
+++ src/fruit.ts
@@ -1,1 +1,17 @@
-import * as PIXI from 'pixi.js';
+import Container = PIXI.Container;
+
+class Fruit extends PIXI.Sprite {
+    constructor(private id: number, parent: Container = null) {
+
+        super(PIXI.Texture.fromFrame(`${id}`))
+        this.scale.set(1.5, 1.5)
+        const me = this
+        this.x = Math.random() * (800 - this.width)
+        this.y = 150 //-Math.random()*100
+        if (parent) {
+            parent.addChild(me)
+        }
+    }
+}
+
+export default Fruit
