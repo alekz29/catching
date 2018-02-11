dea40acb1dfd6bb8e8225f8aa9b725b19cd0d671
alekz29 <konfucjussz29@gmail.com>
2018-02-11 21:47:54 +0100

    creating sprites(fruits) using class

--- src/fruit.ts
+++ src/fruit.ts
@@ -5,11 +5,10 @@ class Fruit extends PIXI.Sprite {
 
         super(PIXI.Texture.fromFrame(`${id}`))
         this.scale.set(1.5, 1.5)
-        const me = this
         this.x = Math.random() * (800 - this.width)
         this.y = 150 //-Math.random()*100
         if (parent) {
-            parent.addChild(me)
+            parent.addChild(this)
         }
     }
 }
